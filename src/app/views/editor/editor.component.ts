e7645b42bf5d3b8ce5bfafa170f8c50256ff596e
Unknown <leon@piloteers.tech>
2020-11-12 23:37:07 +0100

    work on zoom

--- src/app/views/editor/editor.component.ts
+++ src/app/views/editor/editor.component.ts
@@ -8,25 +8,26 @@ import { ResizeEvent } from 'angular-resizable-element';
 })
 export class EditorComponent implements OnInit {
   previewUrl = 'https://sterni-bingo.de';
-  previewStyles = { width: '40vw' };
+  previewStyles = { width: '420px' };
+  breakpoints = [320, 375, 420, 480, 568, 667, 768, 992, 1024, 1280, 1366, 1440, 1680, 1920];
   constructor() {}
 
   ngOnInit(): void {}
 
   onResizeIframe(action): void {
-    const width = document.getElementById('preview').offsetWidth;
-    let newWidth = 300;
+    const width = document.getElementById('preview').offsetWidth + 2;
+    let newWidth = 420;
     switch (action) {
       case 'min':
-        newWidth = width - 100 > 300 ? width - 100 : 300;
-        this.previewStyles.width = `${newWidth}px`;
+        newWidth = this.breakpoints.findIndex((w) => w === width);
+        this.previewStyles.width = `${this.breakpoints?.[newWidth - 1] ? this.breakpoints?.[newWidth - 1] : 320}px`;
         break;
       case 'max':
-        newWidth = width + 100 < window.innerWidth - 400 ? width + 100 : window.innerWidth - 400;
-        this.previewStyles.width = `${newWidth}px`;
+        newWidth = this.breakpoints.findIndex((w) => w === width);
+        this.previewStyles.width = `${this.breakpoints?.[newWidth + 1] ? this.breakpoints?.[newWidth + 1] : 320}px`;
         break;
       case 'phone':
-        this.previewStyles.width = `${400}px`;
+        this.previewStyles.width = `${420}px`;
         break;
       case 'desktop':
         this.previewStyles.width = `${1280}px`;
